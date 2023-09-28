9e115eac2c7a7fcd1e6ef4b34e1a9551b2384111
berkeyucetas <berkeeyucetass@gmail.com>
2023-09-28 20:26:07 +0300

    Trafik Lambası Bir Günde Bir Proje #4
    Bu basit projede timer kullanarak bir trafik lambası uygulaması yaptık

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Trafik_Lambası
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnKirmizi = new System.Windows.Forms.Button();
+            this.btnSari = new System.Windows.Forms.Button();
+            this.btnYesil = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnKirmizi
+            // 
+            this.btnKirmizi.Location = new System.Drawing.Point(57, 13);
+            this.btnKirmizi.Name = "btnKirmizi";
+            this.btnKirmizi.Size = new System.Drawing.Size(160, 167);
+            this.btnKirmizi.TabIndex = 0;
+            this.btnKirmizi.UseVisualStyleBackColor = true;
+            // 
+            // btnSari
+            // 
+            this.btnSari.Location = new System.Drawing.Point(57, 186);
+            this.btnSari.Name = "btnSari";
+            this.btnSari.Size = new System.Drawing.Size(160, 167);
+            this.btnSari.TabIndex = 1;
+            this.btnSari.UseVisualStyleBackColor = true;
+            // 
+            // btnYesil
+            // 
+            this.btnYesil.Location = new System.Drawing.Point(57, 359);
+            this.btnYesil.Name = "btnYesil";
+            this.btnYesil.Size = new System.Drawing.Size(160, 167);
+            this.btnYesil.TabIndex = 2;
+            this.btnYesil.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(276, 575);
+            this.Controls.Add(this.btnYesil);
+            this.Controls.Add(this.btnSari);
+            this.Controls.Add(this.btnKirmizi);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Trafik Lambası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKirmizi;
+        private System.Windows.Forms.Button btnSari;
+        private System.Windows.Forms.Button btnYesil;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Trafik Lambası")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Trafik Lambası")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("81f2f1b4-284e-4886-a57c-46a9052aaf6e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
